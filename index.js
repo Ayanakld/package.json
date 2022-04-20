fbb25ea48d48a1c45d4ec91241a8b8332e6dbc6c
Damir <damir.ps04@gmail.com>
2022-04-20 18:29:40 +0600

    damir commit

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.set("public", path.join(__dirname, "public"));
 app.use("/", require("./routes/root"));
 app.use("/regis", require("./routes/registration"));
 app.use("/login", require("./routes/login"));
-
+app.use("/profile", require("./routes/profile"));
 app.listen(app.get("port"),function(){
     console.log("App started on port http://localhost:" + app.get("port"));
 });
